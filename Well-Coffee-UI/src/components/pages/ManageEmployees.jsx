69a4ac95edba506f75f874ce2f4cc8666a7851d6
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-07-18 14:04:55 -0500

    Add page components to handle navigation routing

---
+++ Well-Coffee-UI/src/components/pages/ManageEmployees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ManageEmployees = () => {
+  return (
+    <h1>ManageEmployees</h1>
+  )
+}
+
+export default ManageEmployees
---
+++ Well-Coffee-UI/src/components/pages/Tutorial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tutorial = () => {
+  return (
+    <h1>Tutorial</h1>
+  )
+}
+
+export default Tutorial
