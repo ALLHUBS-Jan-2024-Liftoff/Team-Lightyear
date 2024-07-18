1c5645dcf2aec25b38d610b5fe21aa73722a06e1
Dominique Gould <dominiquegould42@yahoo.com>
2024-07-17 20:00:31 -0500

    Added classes with empty code

---
+++ WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/models/Account.java
@@ -0,0 +1,4 @@
+package com.team_lightyear.WellCoffeeInventoryAPI.models;
+
+public class Account {
+}
---
+++ WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/repositories/AccountRepository.java
@@ -0,0 +1,4 @@
+package com.team_lightyear.WellCoffeeInventoryAPI.repositories;
+
+public interface AccountRepository {
+}
