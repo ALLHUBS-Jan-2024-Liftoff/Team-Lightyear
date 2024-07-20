945b8111973a100d5f5cac497b72fd13cabf6c7c
Dominique Gould <dominiquegould42@yahoo.com>
2024-07-20 12:57:34 -0500

    added some code to LoginPage jsx

--- Well-Coffee-UI/src/components/pages/LoginPage.jsx
+++ Well-Coffee-UI/src/components/pages/LoginPage.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
+/* 
+  Created by Dominique Gould 
+*/
+
 const LoginPage = () => {
   return (
-    <h1>LoginPage</h1>
+    <div className='container'>
+      <div className='header'>
+        <h2>Welcome Back!</h2>
+        <h1>Log In</h1>
+      </div>
+    {/* Add your login form here */}
+    <div className='loginForm'>
+      {/* Your form inputs */}
+      <form>
+        <div className='inputContainer'>
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+        </div>
+        {/* Will add forgot password later */}
+        <div className='submitContainer'>
+          <button type='submit'>Employee Login</button>
+          <button type='submit'>Manager Login</button>
+        </div>
+
+
+      </form>
+    </div>
+    
+
+    </div>
   )
 }
 
