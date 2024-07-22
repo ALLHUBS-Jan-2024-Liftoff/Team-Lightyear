1ea2cb182539810737418a291e418ce0ec47770d
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-07-21 21:53:04 -0500

    Create CategoryController with CRUD endpoints

--- WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/controllers/CategoryController.java
+++ WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/controllers/CategoryController.java
@@ -1,2 +1,59 @@
-package com.team_lightyear.WellCoffeeInventoryAPI.controllers;public class CategoryController {
+package com.team_lightyear.WellCoffeeInventoryAPI.controllers;
+
+import com.team_lightyear.WellCoffeeInventoryAPI.models.Category;
+import com.team_lightyear.WellCoffeeInventoryAPI.services.CategoryService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping("/new")
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.createCategory(category);
+    }
+
+    @GetMapping("/all")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCategoryById(@PathVariable int id) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable int id, @RequestBody Category category) {
+        Category updatedCategory = categoryService.updateCategory(id, category);
+
+        if (updatedCategory == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable int id) {
+        try {
+            categoryService.deleteCategory(id);
+            return new ResponseEntity<>("Category with ID " + id + " deleted successfully", HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
 }
