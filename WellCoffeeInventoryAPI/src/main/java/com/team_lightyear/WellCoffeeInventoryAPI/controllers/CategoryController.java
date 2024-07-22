e2ef2ddbb4ba0b12237f8f6142dc291d73cc7a09
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-07-21 21:52:33 -0500

    Add CRUD methods to CategoryService for handling categories

---
+++ WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/controllers/CategoryController.java
@@ -0,0 +1,2 @@
+package com.team_lightyear.WellCoffeeInventoryAPI.controllers;public class CategoryController {
+}
--- WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/services/CategoryService.java
+++ WellCoffeeInventoryAPI/src/main/java/com/team_lightyear/WellCoffeeInventoryAPI/services/CategoryService.java
@@ -1,6 +1,14 @@
 package com.team_lightyear.WellCoffeeInventoryAPI.services;
 
+import com.team_lightyear.WellCoffeeInventoryAPI.models.Category;
+import com.team_lightyear.WellCoffeeInventoryAPI.repositories.CategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Deanne Chae
@@ -8,4 +16,39 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category) {
+        category.setDateCreated(LocalDateTime.now());
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(int id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category updateCategory(int id, Category category) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+
+            existingCategory.setName(category.getName());
+
+            return categoryRepository.save(existingCategory);
+        }
+        return null;
+    }
+
+    public void deleteCategory(int id) {
+        if(!categoryRepository.existsById(id)) {
+            throw new EntityNotFoundException("Category with ID " + id + " not found");
+        }
+        categoryRepository.deleteById(id);
+    }
 }
