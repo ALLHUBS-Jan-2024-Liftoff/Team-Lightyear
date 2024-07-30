0ea9aa411d8485a3541076387117aa867ed85aff
cptnez <gtracey24@gmail.com>
2024-07-30 15:24:43 -0500

    Added axios

--- Well-Coffee-UI/src/components/staging/InvoiceHistory.jsx
+++ Well-Coffee-UI/src/components/staging/InvoiceHistory.jsx
@@ -1,66 +1,83 @@
 import { useState } from 'react';
+import axios from 'axios'
 import './InvoiceHistory.css';
 
-const InvoiceHistory = () => {
-  return (
-  <div>
-    <h2>Order History</h2>
-{/*     07252024 Grant's comments-This is for a visual, correlation with actual invoices is next */}
-        <table className="table">
-            <tr>
-                <td>
-                      Invoice Number
-                      <p>1640982</p>
-
-                </td>
-                <td>
-                       Invoice Date
-                       <p>230512</p>
-
-                </td>
-                <td>
-                        Employee Id
-                        <p>15655</p>
-
-                </td>
-                <td>
-                        Vendor
-                        <p>The Bean Shoppe</p>
-
-                </td>
-            </tr>
-                        <tr>
-                            <td>
-                                  <p>1561987</p>
-                            </td>
-                            <td>
-                                   <p>220514</p>
-                            </td>
-                            <td>
-                                    <p>14577</p>
-                            </td>
-                            <td>
-                                    <p>Cold Brew Street</p>
-                            </td>
-                        </tr>
-                        <tr>
-                                                    <td>
-                                                          <p>15561</p>
-                                                    </td>
-                                                    <td>
-                                                           <p>220812</p>
-                                                    </td>
-                                                    <td>
-                                                            <p>15657</p>
-                                                    </td>
-                                                    <td>
-                                                            <p>Coffee & Toffee</p>
-                                                    </td>
-                                                </tr>
-        </table>
-
-</div>
-  )
-  }
+function InvoiceHistory() {
+    const getInvoice = () => {
+        axios.get('//localhost:3306/wellcoffee/tables/invoice')
+        .then(response => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+         })
+    }
+    return (
+        <div className="InvoiceHistory">
+            <button onClick={getInvoice}>Get Invoice</button>
+        </div>
+    )
+}
+// const InvoiceHistory = () => {
+//   return (
+//   <div>
+//     <h2>Order History</h2>
+// {/*     07252024 Grant's comments-This is for a visual, correlation with actual invoices is next */}
+//         <table className="table">
+//             <tr>
+//                 <td>
+//                       Invoice Number
+//                       <p>1640982</p>
+//
+//                 </td>
+//                 <td>
+//                        Invoice Date
+//                        <p>230512</p>
+//
+//                 </td>
+//                 <td>
+//                         Employee Id
+//                         <p>15655</p>
+//
+//                 </td>
+//                 <td>
+//                         Vendor
+//                         <p>The Bean Shoppe</p>
+//
+//                 </td>
+//             </tr>
+//                         <tr>
+//                             <td>
+//                                   <p>1561987</p>
+//                             </td>
+//                             <td>
+//                                    <p>220514</p>
+//                             </td>
+//                             <td>
+//                                     <p>14577</p>
+//                             </td>
+//                             <td>
+//                                     <p>Cold Brew Street</p>
+//                             </td>
+//                         </tr>
+//                         <tr>
+//                                                     <td>
+//                                                           <p>15561</p>
+//                                                     </td>
+//                                                     <td>
+//                                                            <p>220812</p>
+//                                                     </td>
+//                                                     <td>
+//                                                             <p>15657</p>
+//                                                     </td>
+//                                                     <td>
+//                                                             <p>Coffee & Toffee</p>
+//                                                     </td>
+//                                                 </tr>
+//         </table>
+//
+// </div>
+//   )
+//   }
 
   export default InvoiceHistory;
