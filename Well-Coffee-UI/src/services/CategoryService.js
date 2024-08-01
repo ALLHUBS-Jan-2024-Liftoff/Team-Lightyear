c72250b8584fafc7607b16ec09e6dfe74fa6cd02
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-08-01 11:21:31 -0500

    Add 'getAllCategories' function to handle GET requests in 'CategoryService'

--- Well-Coffee-UI/src/services/CategoryService.js
+++ Well-Coffee-UI/src/services/CategoryService.js
@@ -11,3 +11,13 @@ export const createCategory = async (categoryName) => {
     throw error;
   }
 };
+
+export const getAllCategories = async () => {
+  try {
+    const response = await axios.get(`${BASEAPIURL}/all`);
+    return response.data;
+  } catch (error) {
+    console.error("There was an error fetching the categories", error);
+    throw error;
+  }
+};
