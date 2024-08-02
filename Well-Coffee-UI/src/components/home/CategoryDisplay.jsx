0e215690eb76584143dac0ebcaf82b69f71ceec4
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-08-02 10:52:31 -0500

    Add ternary conditionals in 'CategoryDisplay' to render correct category and item information when no data is present

--- Well-Coffee-UI/src/components/home/CategoryDisplay.jsx
+++ Well-Coffee-UI/src/components/home/CategoryDisplay.jsx
@@ -10,43 +10,56 @@ const CategoryDisplay = ({ categories }) => {
       <Container className='mt-5'>
         <h1 className='text-center'>Inventory</h1>
         <Accordion alwaysOpen className='mt-4'>
-          {categories.map((category) => (
-            <Accordion.Item key={category.id} eventKey={category.id.toString()}>
-              <Accordion.Header>{category.name}</Accordion.Header>
+          {categories.length === 0 ? (
+            <Accordion.Item>
+              <Accordion.Header>No categories available</Accordion.Header>
               <Accordion.Body>
-                <Table striped bordered hover responsive>  
-                  <thead>
-                    <tr>
-                      <th>Status</th>
-                      <th>Name</th>
-                      <th>Description</th>
-                      <th>Location</th>
-                      <th>Quantity</th>
-                      <th>Item Cost</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {category.items.map((item, index) => (
-                      <tr key={index}>
-                        <td>{item.status}</td>
-                        <td>{item.name}</td>
-                        <td>{item.description}</td>
-                        <td>{item.location}</td>
-                        <td>{item.quantity}</td>
-                        <td>{item.cost}</td>
-                        <td>
-                          <ItemCardModal />{' '}
-                          <UpdateItemModal />{' '}
-                          <Button variant='outline-danger'>Delete</Button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
+                <div>⬇️</div>
               </Accordion.Body>
             </Accordion.Item>
-          ))}
+          ) : ( 
+            categories.map((category) => (
+              <Accordion.Item key={category.id} eventKey={category.id.toString()}>
+                <Accordion.Header>{category.name}</Accordion.Header>
+                <Accordion.Body>
+                  {category.items.length == 0 ? (
+                    <div>No items available</div>
+                  ) : (
+                    <Table striped bordered hover responsive>  
+                      <thead>
+                        <tr>
+                          <th>Status</th>
+                          <th>Name</th>
+                          <th>Description</th>
+                          <th>Location</th>
+                          <th>Quantity</th>
+                          <th>Item Cost</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {category.items.map((item, index) => (
+                          <tr key={index}>
+                            <td>{item.status}</td>
+                            <td>{item.name}</td>
+                            <td>{item.description}</td>
+                            <td>{item.location}</td>
+                            <td>{item.quantity}</td>
+                            <td>{item.cost}</td>
+                            <td>
+                              <ItemCardModal />{' '}
+                              <UpdateItemModal />{' '}
+                              <Button variant='outline-danger'>Delete</Button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  )}
+                </Accordion.Body>
+              </Accordion.Item>
+            ))
+          )}
         </Accordion>
       </Container>
     </>
