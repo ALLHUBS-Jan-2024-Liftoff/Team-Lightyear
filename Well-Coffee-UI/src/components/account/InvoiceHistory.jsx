7116734772505d1ca5d6e1ff0d0c6c03c7e72a47
cptnez <gtracey24@gmail.com>
2024-08-07 15:56:20 -0500

    List items for first array of invoices. Still fixing up the loop

--- Well-Coffee-UI/src/components/account/InvoiceHistory.jsx
+++ Well-Coffee-UI/src/components/account/InvoiceHistory.jsx
@@ -12,7 +12,7 @@ const InvoiceHistory = () => {
 
   const [invoices] = useState([
     {
-      accountId: 1562,
+      id: 1562,
       items: [
         {
           invoiceNumber: 12012,
@@ -26,65 +26,71 @@ const InvoiceHistory = () => {
         }
       ]
     },
-    {
-      accountId: 1648,
-      items: [
-        {
-          invoiceNumber: 16489,
-          invoiceDate: 20110615,
-          vendor: 'The Coffee Fiend',
-        },
-        {
-          invoiceNumber: 18499,
-          invoiceDate: 20110602,
-          vendor: 'Java Time',
-        }
-      ]
-    },
-    {
-      accountId: 4987,
-      items: [
-        {
-          invoiceNumber: 1546,
-          invoiceDate: 20121204,
-          vendor: 'Coffee Spot',
-        },
-        {
-          invoiceNumber: 1894,
-          invoiceDate: 20130606,
-          vendor: 'The Coffee Fiend',
-        }
-      ]
-    }
+//     {
+//       id: 1648,
+//       items: [
+//         {
+//           invoiceNumber: 16489,
+//           invoiceDate: 20110615,
+//           vendor: 'The Coffee Fiend',
+//         },
+//         {
+//           invoiceNumber: 18499,
+//           invoiceDate: 20110602,
+//           vendor: 'Java Time',
+//         }
+//       ]
+//     },
+//     {
+//       id: 4987,
+//       items: [
+//         {
+//           invoiceNumber: 1546,
+//           invoiceDate: 20121204,
+//           vendor: 'Coffee Spot',
+//         },
+//         {
+//           invoiceNumber: 1894,
+//           invoiceDate: 20130606,
+//           vendor: 'The Coffee Fiend',
+//         }
+//       ]
+//     }
   ]);
 
   return (
 <>
-       <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-          View Invoices
-            </Dropdown.Toggle>
-                 <Dropdown.Menu>
-               {invoices.map((invoice) => (
-                    <Dropdown.Item key={invoice.accountId} eventKey={invoice.invoiceNumber} onClick={() => {
+{/*        <Dropdown> */}
+{/*             <Dropdown.Toggle variant="success" id="dropdown-basic"> */}
+{/*           View Invoices */}
+{/*             </Dropdown.Toggle> */}
+{/*                  <Dropdown.Menu> */}
+{/*                {invoices.map((invoice) => ( */}
+{/*                     <Dropdown.Item key={invoice.accountId} eventKey={invoice.invoiceNumber} onClick={() => { */}
 
-                         newWindow.document.body.innerHTML = '<a href="invoiceNumber">{invoice.invoiceNumber}</a>';
-                   }}>{invoice.accountId}</Dropdown.Item>
-                         ))}
-                 </Dropdown.Menu>
-         </Dropdown>
+{/*                          newWindow.document.body.innerHTML = '<a href="invoiceNumber">{invoice.invoiceNumber}</a>'; */}
+{/*                    }}>{invoice.accountId}</Dropdown.Item> */}
+{/*                          ))} */}
+{/*                  </Dropdown.Menu> */}
+{/*          </Dropdown> */}
 
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+      <Button variant="secondary" onClick={handleShow}>
+        Invoice History
       </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-                       {invoices.map((invoice) => (
-                            <Modal.Title key={invoice.accountId} eventKey={invoice.invoiceNumber} onClick={() => {
-                           }}>Invoice# {invoice.accountId}</Modal.Title>
-                                 ))}
+            <Modal.Title>Invoice History</Modal.Title>
         </Modal.Header>
-        <Modal.Body></Modal.Body>
+        <Modal.Body>
+        {invoices.map(invoice =>
+               (
+                <ul key={invoice.id}>
+                <li>Invoice# {invoice.items[0].invoiceNumber}</li>
+                <li>Date: {invoice.items[0].invoiceDate}</li>
+                <li>Vendor: {invoice.items[0].vendor}</li>
+                </ul>
+                ))}
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
