b41e2516136d889e0e58051eeaeb8d4d61b11009
cptnez <gtracey24@gmail.com>
2024-08-14 19:52:40 -0500

    Working thought the view

--- Well-Coffee-UI/src/components/account/InvoiceHistory.jsx
+++ Well-Coffee-UI/src/components/account/InvoiceHistory.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import { Table, Button, Modal } from "react-bootstrap";
 import React from "react";
-import { DummyInvoices } from "/src/assets/dummydata/DummyInvoices"
-import { getAllInvoices } from '../../services/InvoiceService';
+import { DummyInvoices } from "/src/assets/dummydata/DummyInvoices";
+import { getAllInvoices } from "../../services/InvoiceService";
 
-const InvoiceHistory = ( { invoices } ) => {
+const InvoiceHistory = ({ invoices }) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
 
   return (
     <>
@@ -30,15 +29,17 @@ const InvoiceHistory = ( { invoices } ) => {
                   <th>Vendor</th>
                 </tr>
               </thead>
-                   <tbody>
-{/*                 {DummyInvoices.map((invoice, index) => ( */}
-{/*                       <tr key={index}> */}
-{/*                         <td>{invoice.invoice_number}</td> */}
-{/*                         <td>{invoice.invoice_date}</td> */}
-{/*                         <td>{invoice.vendor}</td> */}
-{/*                       </tr> */}
-{/*               ))} */}
-              </tbody>
+              {invoices.length === 0 ? (
+                <tbody>No data!</tbody>
+              ) : (
+                invoices.map((invoice) => (
+                  <tr>
+                    <td>{invoice.invoice_number}</td>
+                    <td>{invoice.invoice_date}</td>
+                    <td>{invoice.vendor}</td>
+                  </tr>
+                ))
+              )}
             </Table>
           </div>
         </Modal.Body>
