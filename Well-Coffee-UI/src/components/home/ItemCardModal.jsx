cffe5399d1224fbde7e6c3444f03369c9dc46f72
cptnez <gtracey24@gmail.com>
2024-08-15 16:06:27 -0500

    Added code to updateItem to include comment. Added code to ItemCardModal to handle the change of comment

--- Well-Coffee-UI/src/components/home/ItemCardModal.jsx
+++ Well-Coffee-UI/src/components/home/ItemCardModal.jsx
@@ -1,12 +1,50 @@
 import { useState } from 'react';
 import defaultImage from '../../assets/images/no-image.png';
-import { Button, Modal, Badge, Card, ListGroup } from 'react-bootstrap';
+import { updateItem } from '../../services/ItemService';
+import { Button, Modal, Badge, Card, ListGroup, Form } from 'react-bootstrap';
 
 const ItemCardModal = ({ item }) => {
   const [show, setShow] = useState(false);
+  const [message, setMessage] = useState("");
+    const [formData, setFormData] = useState({
+    comment: item.comment
+    });
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+    setFormData({
+comment: formData.comment
+    });
+    setMessage("");
+  };
+
   const handleShow = () => setShow(true);
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((oldData) => ({
+        ...oldData,
+        [name]: value
+      }));
+    }
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newData = {
+        comment: formData.comment
+        };
+
+        try {
+          await updateItem(item.id, newData);
+          setMessage("Item updated successfully");
+          setTimeout(() => {
+            handleClose();
+          }, 1000);
+        } catch (error) {
+          setMessage("There was an error updating the item. Please try again.");
+        }
+      };
 
   return (
     <>
@@ -31,11 +69,23 @@ const ItemCardModal = ({ item }) => {
             <Card.Body>
               <Card.Img variant="top" src={item.image || defaultImage} />
             </Card.Body>
+            <Form onSubmit={handleSubmit} id="updateItemForm">
+                        <Form.Group className="mb-3" controlId="itemComment">
+                          <Form.Control
+                            as="textarea"
+                            rows={4}
+                            placeholder="Enter comment"
+                            name="comment"
+//                             value={formData.comment}
+                            onChange={handleChange}
+                          />
+                        </Form.Group>
+                                  <Button variant="secondary" type="submit" form="updateItemForm" onClick={handleClose}>
+                                    Submit
+                                  </Button>
+                                  </Form>
             <ListGroup variant='flush'>
-              <ListGroup.Item>Description: {item.description}</ListGroup.Item>
-              <ListGroup.Item>Location: {item.location}</ListGroup.Item>
-              <ListGroup.Item>Item Cost: ${item.price}</ListGroup.Item>
-              <ListGroup.Item>Comment: {item.comment}</ListGroup.Item>
+              <ListGroup.Item>Comment: {formData.comment}</ListGroup.Item>
             </ListGroup>
           </Card>
         </Modal.Body>
