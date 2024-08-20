155ff490bac3d3a8d75b04ef8be4988a266a34e9
Dominique Gould <dominiquegould42@yahoo.com>
2024-08-20 11:38:02 -0500

    Corrected route to OrderPage

--- Well-Coffee-UI/src/App.jsx
+++ Well-Coffee-UI/src/App.jsx
@@ -5,7 +5,7 @@ import { Route,
   Navigate } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import AccountPage from "./components/pages/AccountPage";
-import OrderPage from "./components/pages/OrderPage";
+import OrderFormPage from "./components/orderForm/OrderFormPage";
 import ManageEmployees from "./components/pages/ManageEmployees";
 import DailyReport from "./components/pages/DailyReport";
 import Tutorial from "./components/pages/Tutorial";
@@ -32,7 +32,7 @@ function App() {
       ) : (
         <>
           <Route path="/home" element={<HomePage />} />
-          <Route path="/order" element={<OrderPage />} />
+          <Route path="/order" element={<OrderFormPage />} />
           <Route path="/manage" element={<ManageEmployees />} />
           <Route path="/report" element={<DailyReport />} />
           <Route path="/tutorial" element={<Tutorial />} />
@@ -48,64 +48,7 @@ function App() {
 
 
 
-  // return (
-  //     <nav>
-  //       {!authenticated ? (
-  //         <>
-  //               <NavigationBar /> 
-  //               {/* <NavigationRoutes />  */}
-  //         </>
-  //       ) : (
-  //       <>
-
-  //       <Link to="/home">Home</Link>
-  //       <Link to="/tutorial">Tutorial</Link>
-  //       <Link to="/report">Daily Report</Link>
-  //       <Link to="/manage">Manage Employees</Link>
-  //       <Link to="/order">Order</Link>
-  //       <Link to="/logout">Logout</Link>
-  //       </>
-  //       )}
-  //       <Routes>
-  //         {/* Public Routes */}
-  //         <Route 
-  //         path="/login" 
-  //         element={<LoginPage setAuthenticated={setAuthenticated} />} 
-  //         />
-  //         <Route 
-  //         path="tutorial" 
-  //         element={<Tutorial />} 
-  //         />
-  //         <Route 
-  //         path="/register" 
-  //         element={<Register />} 
-  //         />
-  //           {/* Private Routes */}
-  //           {authenticated ? (
-              
-  //             <>
-  //             <NavigationRoutes />
-
-  //             <Route path="/tutorial" element={<Tutorial />} />
-  //             <Route path="/home" element={<HomePage />} />
-  //             <Route path="/report" element={<DailyReport />} />
-  //             <Route path="/manage" element={<ManageEmployees />} />
-  //             <Route path="/order" element={<OrderPage />} />
-  //             {/* <NavigationRoutes /> */}
-
-  //               </>
-  //           ) : (
-              
-  //             /* Redirect non-authenticated users to login */
-
-  //             <Route path="*" element={<Navigate to="/login" replace />} />
-
-  //           )}
-
-
-  //       </Routes>
-  //     </nav>
-  // );
+  
 }
 
 export default App;
