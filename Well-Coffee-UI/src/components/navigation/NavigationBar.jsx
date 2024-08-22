25a60fb20ea9dcb68e527f65e2d978fe18f3aec1
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-08-21 22:53:41 -0500

    Clean up code

--- Well-Coffee-UI/src/components/navigation/NavigationBar.jsx
+++ Well-Coffee-UI/src/components/navigation/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import logo from '../../assets/images/logo-transparent.png';
 import './NavigationBar.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavigationBar = ({ authenticated }) => {
